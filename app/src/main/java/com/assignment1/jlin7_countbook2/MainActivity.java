d84be8831943d1367160a21c4502a6a47a7f57e5
jstl1025 <jlin7@ualberta.ca>
2017-10-02 11:05:04 -0600

    save button
    -after pressing save button, show toast
    -add cardview, recyclerview dependencies in gradle

--- app/src/main/java/com/assignment1/jlin7_countbook2/MainActivity.java
+++ app/src/main/java/com/assignment1/jlin7_countbook2/MainActivity.java
@@ -1,12 +1,16 @@
 package com.assignment1.jlin7_countbook2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_CODE=1;
+    private String[] name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
         addBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent (v.getContext(), CreateCounterActivity.class);
-                startActivity(intent);
-                //startActivityForResult(intent, REQUEST_CODE);
+                startActivityForResult(intent, REQUEST_CODE);
 
 
                 }
             });
-        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            if(data.hasExtra("counterName")){
+                Context context = getApplicationContext();
+                CharSequence text  = ("Counter "+data.getExtras().getString("counterName")+" created");
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        }
+    }
 }
